58db79b5f5f889c292f20f98f6f76b6519c842ba
h7x4 <h7x4@nani.wtf>
2023-07-23 20:57:00 +0200

    flake.nix: add build instructions for package

--- flake.nix
+++ flake.nix
@@ -17,11 +17,29 @@
         pkgs = nixpkgs.legacyPackages.${system};
       in
       {
-        devShell = pkgs.mkShell {
+        devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
             gnumake
             go_1_19
           ];
+        };
+
+        packages.default = pkgs.buildGoModule rec {
+          pname = "github_exporter";
+          version = "unstable";
+          src = ./.;
+
+          ldflags =
+            let
+              t = "github.com/promhippie/github_exporter/pkg/version";
+            in [
+              "-s" "-w"
+              "-X ${t}.String=${version}"
+              "-X ${t}.Revision=HEAD"
+              "-X ${t}.Date=unknown"
+            ];
+
+          vendorSha256 = "sha256-CdT4noTYbax+enN81AQTUDjlJ+M4pvoiPR6iUzYURMI=";
         };
       }
     );
